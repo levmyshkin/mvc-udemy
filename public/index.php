a73803cbfffbcee1343d47730d1f6af1f13d0e5b
levmyshkin <levmyshkin89@gmail.com>
2016-05-01 14:43:41 +0600

    Add Route class

--- public/index.php
+++ public/index.php
@@ -1,2 +1,19 @@
 <?php
-  print $_SERVER['QUERY_STRING'];
+
+/**
+ * Front Controller
+ */
+
+/**
+ * Routing
+ */
+require '../Core/Router.php';
+
+$router = new Router();
+
+// Add the routes.
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+
+
