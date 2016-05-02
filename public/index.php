03783d11247b9de6618a52135a26b8c498e52aa0
levmyshkin <levmyshkin89@gmail.com>
2016-05-02 11:05:09 +0600

    Add regexp

--- public/index.php
+++ public/index.php
@@ -14,15 +14,18 @@ $router = new Router();
 // Add the routes.
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
-$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+$router->add('{conroller}/{action}');
+$router->add('admin/{action}/{conroller}');
 
 // Match the requested route.
 $url = $_SERVER['QUERY_STRING'];
 
 if ($router->match($url)) {
   print '<pre>';
-  var_dump($router->getParams());
+  print htmlspecialchars(print_r($router->getRoutes(), true));
   print '</pre>';
+
+  var_dump($router->getParams());
 }
 else {
   print 'No route found for URL ' . $url;
